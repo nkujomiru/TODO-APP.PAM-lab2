22452edb4c31ad8e2f18fc025fa5622eb1af267f
Jomiru Nicolai <nkujomiru@gmail.com>
2020-10-11 03:13:14 +0300

    Add navigationand separeate screens

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import LandingScreen from './screens/Landing';
+import HomeScreen from './screens/Home';
+
+const RootStack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <RootStack.Navigator initialRouteName="Home">
+        <RootStack.Screen name="Landing" component={LandingScreen} />
+        <RootStack.Screen name="Home" component={HomeScreen} />
+      </RootStack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ screens/Landing/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+const LandingScreen = ({navigation}) => (
+	<View style={styles.container}>
+		<Text>Public Landing Screen</Text>
+		<Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+	</View>
+);
+
+export default LandingScreen;
